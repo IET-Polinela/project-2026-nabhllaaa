8967eaafe3764f644277030a32cc8afda6c344b6
nbhllaaa <nabhillaayularasati@gmail.com>
2026-04-05 21:34:43 +0700

    Lab 3

--- iet_2026_24782022/iet_2026_24782022/urls.py
+++ iet_2026_24782022/iet_2026_24782022/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('add/', views.add_report, name='add_report'),
     path('edit/<int:report_id>/', views.edit_report, name='edit_report'),
     path('delete/<int:report_id>/', views.delete_report, name='delete_report'),
-    path('status/process/<int:report_id>/', views.update_status_process, name='status_process'),
-    path('status/done/<int:report_id>/', views.update_status_done, name='status_done'),
 ]
