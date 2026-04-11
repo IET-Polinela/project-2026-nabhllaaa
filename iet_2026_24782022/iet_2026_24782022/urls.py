3885ceaa9c4940d349f94c548e110b9273a3a0d9
nbhllaaa <nabhillaayularasati@gmail.com>
2026-04-11 18:33:58 +0700

    Lab 4 - CBV

--- iet_2026_24782022/iet_2026_24782022/urls.py
+++ iet_2026_24782022/iet_2026_24782022/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add/', views.ReportCreateView.as_view(), name='add_report'),
     path('edit/<int:pk>/', views.ReportUpdateView.as_view(), name='edit_report'),
     path('delete/<int:pk>/', views.ReportDeleteView.as_view(), name='delete_report'),
+    path('detail/<int:pk>/', views.ReportDetailView.as_view(), name='report_detail'),
 ]
